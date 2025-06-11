aa2e4ad978d9f468a50b160d87a6dc64fc35b791
BoKalebsson <bo_kalebsson@hotmail.com>
2025-06-11 16:46:00 +0200

    Updated with some more exercises.

--- src/main/java/io/github/bokalebsson/Exercises.java
+++ src/main/java/io/github/bokalebsson/Exercises.java
@@ -6,14 +6,14 @@ public class Exercises {
 
     public static void main(String[] args) {
 
-        /*Create a String with your first name as content. Print out the String
+/*      Create a String with your first name as content. Print out the String
         object to the console.*/
 
         /*String firstName = "Bo";
         System.out.println(firstName);*/
 
 
-        /*Create two Strings. One with your first name and the other with your
+/*      Create two Strings. One with your first name and the other with your
         last name. Print out both Strings to the console with a ” ” separating
         them.*/
 
@@ -22,7 +22,7 @@ public class Exercises {
         System.out.println(firstName + " " + lastName);*/
 
 
-       /* Create two variables with decimal values. Create a third decimal
+/*      Create two variables with decimal values. Create a third decimal
         variable called result. Assign the sum of the two first variables to
         result.*/
 
@@ -32,7 +32,7 @@ public class Exercises {
         System.out.println(result);*/
 
 
-        /*Create a class called Car. The class should contain the following
+/*      Create a class called Car. The class should contain the following
         information. Car brand, registration number, max speed, owner name
         and one more attribute of your choice. Instantiate an Object of the
         class and assign values to the object. */
@@ -45,8 +45,8 @@ public class Exercises {
             String color;
             boolean canParkCorrectly;*/
 
-//            Method for printing out the information about the Car.
-/*            public void printInfo() {
+
+/*          public void printInfo() {
                 System.out.println("Brand: " + brand);
                 System.out.println("Registration Number: " + regNumber);
                 System.out.println("Max speed: " + maxSpeed);
@@ -56,7 +56,7 @@ public class Exercises {
             }
         }*/
 
-//        Instantiating the Car Class.
+
 /*          Car carNumber1 = new Car();
             carNumber1.brand = "BMW";
             carNumber1.regNumber = "ABC 123";
@@ -68,7 +68,7 @@ public class Exercises {
             carNumber1.printInfo();*/
 
 
-        /* Make an integer called score and assign a value to it.
+/*      Make an integer called score and assign a value to it.
         Create an if-statement that should print out ”Hurray, You passed!” if the score is above or equals to 65.
         If the score is Equal or between 55 and 64 it should print ”You are almost there.”
         If score is below 55 it should print ”Sorry, You did not pass.”*/
@@ -82,7 +82,8 @@ public class Exercises {
                 System.out.println("Sorry, You did not pass.");
             }*/
 
-        /* Being a developer you need to learn how to search for information on
+
+/*      Being a developer you need to learn how to search for information on
         the Internet. Open up a web browser and search for ”java input from
         user”. • Use your new knowledge to take a String as input from the user and store it in
         a String object.*/
@@ -92,10 +93,90 @@ public class Exercises {
         String name = scanner.nextLine();
         System.out.println(name);*/
 
-        System.out.println("You are here!");
+
+/*      Write a Java program to print 'Hello' on screen and then print your name on
+        a separate line.
+        Expected result:
+        Hello
+        Ali!*/
+
+/*        String yourName = "Bo Kalebsson";
+        System.out.println("Hello");
+        System.out.println(yourName + "!");*/
+
+
+/*        Create a program that takes a year as input from user and print if it’s leap
+        year or not.*/
+
+/*        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a year: ");
+        int year = scanner.nextInt();
+
+            if (year % 4 == 0) {
+                if (year % 100 == 0) {
+                    if (year % 400 == 0) {
+                        System.out.println(year + " is a leap year.");
+                    } else {
+                        System.out.println(year + " is not a leap year.");
+                    }
+                } else {
+                    System.out.println(year + " is a leap year.");
+                }
+            } else {
+                System.out.println(year + " is not a leap year.");
+            }
+
+            scanner.close();*/
+
+
+/*      Write a Java program to print the sum/multiplication/division and
+        subtraction of two numbers
+        Expected result:
+        45 + 11 = 56
+        12 * 4 = 48
+        24 / 6 = 4
+        55 – 12 = 43*/
+
+/*        int number1 = 69;
+        int number2 = 5;
+
+        System.out.println(number1 + " + " + number2 + " = " + (number1 + number2));
+        System.out.println(number1 + " * " + number2 + " = " + (number1 * number2));
+        if (number2 != 0) {
+            double result = (double) number1 / number2;
+            System.out.println(number1 + " / " + number2 + " = " + result);
+        } else {
+            System.out.println("Division by 0 is not allowed.");
+        }
+
+        System.out.println(number1 + " - " + number2 + " = " + (number1 - number2));*/
+
+
+/*      Write a Java program that prints the average of three numbers
+        Expected result:
+        (23 + 11 + 77) / 3 = 37*/
+
+/*        int number1 = 23;
+        int number2 = 11;
+        int number3 = 77;
+        int average = ((number1 + number2 + number3) / 3);
+
+        System.out.println(number1 + " + " + number2 + " + " + number3 + " / 3 = " +average);*/
+
+
+/*      Create a program that asks user to input his/her name and store it in a
+        variable instead of having fixed name. Then print ‘Hello username’ where
+        username is what you got from user as input.*/
+
+/*        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String username = scanner.nextLine().trim();
+        System.out.println("Hello " + username);*/
+
+
+/*        Create a program that asks user to input two numbers and print the
+        sum/multiplication/division and subtraction of given numbers*/
 
     }
-
 }
-
-
