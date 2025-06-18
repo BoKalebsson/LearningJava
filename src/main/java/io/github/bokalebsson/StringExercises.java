c5cfe2b6395925e87f191122d824fc18b0d8d101
BoKalebsson <bo_kalebsson@hotmail.com>
2025-06-18 13:31:37 +0200

    Solved some String Exercises.

--- src/main/java/io/github/bokalebsson/StringExercises.java
+++ src/main/java/io/github/bokalebsson/StringExercises.java
@@ -9,6 +9,33 @@ public class StringExercises {
 /*        String text = "Java";
         System.out.println("Length of string: " + text.length());*/
 
+/*        What char is at index position 6 in the following String:
+        "Long example sentence" ?*/
+
+/*        String text = "Long example sentence";
+        System.out.println("The letter at position 6 is: " +text.charAt(6));*/
+
+
+/*        What is the index position of 'o' in the following String:
+        "Even longer example sentence" ?*/
+
+/*        String text = "Even longer example sentence";
+        System.out.println("The index position of 'o' is: " + text.indexOf("o"));*/
+
+
+/*        Given the following String: "Ok this is not as long!"
+        create a substring of only "not as long" (excluding the
+        exclamation point) and print it out.*/
+
+/*        String text = "Ok this is not as long!";
+        int start = text.indexOf("not");
+        System.out.println("Index of 'not': " + start);
+        int end = text.indexOf("!");
+        System.out.println("Index of '!': " + end);
+        String result = text.substring(start, end);
+        System.out.println("The substring: " + result);*/
+
+
 
 
 
