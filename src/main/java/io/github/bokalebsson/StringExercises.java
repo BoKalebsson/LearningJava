698487e6d3102e1a96365b20b221a5952f03e154
BoKalebsson <bo_kalebsson@hotmail.com>
2025-06-18 13:05:33 +0200

    Added String Exercises.

---
+++ src/main/java/io/github/bokalebsson/StringExercises.java
@@ -0,0 +1,17 @@
+package io.github.bokalebsson;
+
+public class StringExercises {
+
+    public static void main(String[] args) {
+
+/*        What is the length of the String: "Java" ? */
+
+/*        String text = "Java";
+        System.out.println("Length of string: " + text.length());*/
+
+
+
+
+    }
+
+}
