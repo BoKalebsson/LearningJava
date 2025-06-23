4bcd0ab26f6643c14bac948bf8fc63a3a901dbed
BoKalebsson <bo_kalebsson@hotmail.com>
2025-06-23 11:49:26 +0200

    Solved some Array Exercises.

--- src/main/java/io/github/bokalebsson/ArrayExercises.java
+++ src/main/java/io/github/bokalebsson/ArrayExercises.java
@@ -1,8 +1,22 @@
 package io.github.bokalebsson;
 
+import java.util.Arrays;
+
 public class ArrayExercises {
 
     public static void main(String[] args) {
+
+/*        Write a program which will store elements in an array of type ‘int’ and
+        print it out. Expected output: 11 23 39 etc.*/
+
+/*        int[] numbers = {1, 2, 3, 4, 5};
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }*/
+
+
+
 
 
 
