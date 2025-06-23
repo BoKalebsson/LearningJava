884897b84c23cafc8a697d33ef75b6a6f069c486
BoKalebsson <bo_kalebsson@hotmail.com>
2025-06-23 14:35:32 +0200

    Solved some Array Exercises.

--- src/main/java/io/github/bokalebsson/ArrayExercises.java
+++ src/main/java/io/github/bokalebsson/ArrayExercises.java
@@ -1,5 +1,7 @@
 package io.github.bokalebsson;
 
+import java.util.Arrays;
+
 public class ArrayExercises {
 
     public static void main(String[] args) {
@@ -24,7 +26,7 @@ public class ArrayExercises {
         int index = indexOf(numbers, number);
         System.out.println("Index position of number " + number + " is: " + index);
 
-    }
+
 
     public static int indexOf(int[] array, int value) {
 
@@ -35,6 +37,28 @@ public class ArrayExercises {
         }
         return -1;
     }*/
+
+
+/*        Write a program which will sort string array.
+        Expected output: String array: [Paris, London, New York, Stockholm]
+        Sort string array: [London, New York, Paris, Stockholm]*/
+
+/*        String[] cities = {"Paris", "London", "New York", "Stockholm"};
+        System.out.println("String array: " + Arrays.toString(cities));
+        Arrays.sort(cities);
+        System.out.println("Sorted String array: " + Arrays.toString(cities));*/
+
+
+/*        Write a program which will copy the elements of one array into another
+        array. Expected output: Elements from first array: 1 15 20
+        Elements from second array: 1 15 20*/
+
+/*        int[] array1 = {1, 2, 3, 4, 5};
+        int[] array2 = Arrays.copyOf(array1, array1.length);
+        System.out.println("Elements from first array: " +Arrays.toString(array1));
+        System.out.println("Elements from second array: " +Arrays.toString(array2));*/
+
+
 
 
 
