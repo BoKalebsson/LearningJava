67bf5fbbc8bebdf49c0d5c997dc08012823a3c2f
BoKalebsson <bo_kalebsson@hotmail.com>
2025-07-30 15:02:43 +0200

    Solved some collection exercises.

--- src/main/java/io/github/bokalebsson/CollectionExercises.java
+++ src/main/java/io/github/bokalebsson/CollectionExercises.java
@@ -8,27 +8,31 @@ public class CollectionExercises {
 
     public static void main(String[] args) {
 
-        /*Create a new list and populate it with the days of the week. Lastly, print the out the list.*/
+/*        *//* Create a new list and populate it with the days of the week. Lastly, print the out the list.*//*
 
-        ArrayList<DayOfWeek> week = new ArrayList<>();
         ArrayList<DayOfWeek> week2 = new ArrayList<>();
-
-        // Add days with for-each loop:
-        for (DayOfWeek day : DayOfWeek.values()){
-            week.add(day);
-        }
-
-        // Print each day with for-each loop:
-        System.out.println("-- Days of the week with for each-loop --");
-        for (DayOfWeek day : week){
-            System.out.println(day);
-        }
 
         // Add all the values from the enum-collection to our list:
         Collections.addAll(week2, DayOfWeek.values());
 
         System.out.println("-- Days of the week with collection method --");
-        System.out.println(week2);
+        System.out.println(week2);*/
+
+/*        *//*Create a new list and populate it with the days of the week. Lastly, iterate through the list
+        and print out each element separately.*//*
+
+        ArrayList<DayOfWeek> week = new ArrayList<>();
+
+        // Iterate and add all days with for-each loop:
+        for (DayOfWeek day : DayOfWeek.values()){
+            week.add(day);
+        }
+
+        // Iterate through each day with for-each loop:
+        System.out.println("-- Days of the week with for each-loop --");
+        for (DayOfWeek day : week){
+            System.out.println(day);
+        }*/
 
 
 
