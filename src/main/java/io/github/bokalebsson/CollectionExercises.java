a5d715fb267f587605f6d128e318e8111d425ccd
BoKalebsson <bo_kalebsson@hotmail.com>
2025-07-30 14:45:42 +0200

    Added file for Collection exercises.

--- src/main/java/io/github/bokalebsson/CollectionExercises.java
+++ src/main/java/io/github/bokalebsson/CollectionExercises.java
@@ -1,4 +1,37 @@
 package io.github.bokalebsson;
 
+import java.time.DayOfWeek;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class CollectionExercises {
+
+    public static void main(String[] args) {
+
+        /*Create a new list and populate it with the days of the week. Lastly, print the out the list.*/
+
+        ArrayList<DayOfWeek> week = new ArrayList<>();
+        ArrayList<DayOfWeek> week2 = new ArrayList<>();
+
+        // Add days with for-each loop:
+        for (DayOfWeek day : DayOfWeek.values()){
+            week.add(day);
+        }
+
+        // Print each day with for-each loop:
+        System.out.println("-- Days of the week with for each-loop --");
+        for (DayOfWeek day : week){
+            System.out.println(day);
+        }
+
+        // Add all the values from the enum-collection to our list:
+        Collections.addAll(week2, DayOfWeek.values());
+
+        System.out.println("-- Days of the week with collection method --");
+        System.out.println(week2);
+
+
+
+
+    }
 }
--- src/main/java/io/github/bokalebsson/Main.java
+++ src/main/java/io/github/bokalebsson/Main.java
@@ -1,8 +1,12 @@
 package io.github.bokalebsson;
 
+import java.time.DayOfWeek;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Main {
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
 
 
     }
